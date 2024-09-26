e279bb975cf9f1cc381d51f7aa8325960e5a6e12
Simonf <simon.fcbrt@gmail.com>
2024-09-26 15:53:23 -0700

    feat: print at beginning

--- activity_decay_function.py
+++ activity_decay_function.py
@@ -1,6 +1,18 @@
 import math
 import numpy as np
 
+# Do a function that do a print of "Activity Decay " in the terminal but in very large and with a frame arount 
+
+def print_activity_decay():
+    message = "Activity Decay Calculator"
+    border = "*" * (len(message) + 16)
+    print("\n")
+    print(border)
+    print(f"******* {message} *******")
+    print(border, "\n")
+    return
+
+
 def calculate_activity(A0, t, half_life):
     """
     Calcule l'activité radioactive actuelle.
--- main.py
+++ main.py
@@ -2,21 +2,10 @@ from datetime import datetime
 import half_time_lib
 import math
 import numpy as np
-
-# def calculate_activity(A0, t, half_life):
-#     """
-#     Calcule l'activité radioactive actuelle.
-#     :param A0: activité initiale (float)
-#     :param t: temps écoulé (en années) (float)
-#     :param half_life: demi-vie de l'isotope (en années) (float)
-#     :return: activité radioactive actuelle (float)
-#     """
-#     return np.float32(A0 * (0.5 ** (t / half_life)))
-
-import activity_decay_function 
-
+import activity_decay_function as activity_func 
 
 if __name__ == "__main__":    
+    activity_func.print_activity_decay()
     isotope = input("Entrez le nom de l'isotope (ex. Cesium-137, Cobalt-60) : ")
     half_life = half_time_lib.get_half_life(isotope)
 
@@ -29,7 +18,7 @@ if __name__ == "__main__":
         current_date = datetime.strptime(current_date_str, "%Y-%m-%d %H:%M:%S")
         t = (current_date - initial_date).total_seconds() / (365.25 * 24 * 3600)  # Convert seconds to years
 
-        current_activity = activity_decay_function.calculate_activity(A0, t, half_life)
+        current_activity = activity_func.calculate_activity(A0, t, half_life)
         print(f"L'activité radioactive actuelle de {isotope} après {t:.2f} ans est de {current_activity} Bq.")
     else:
         print("Isotope non trouvé dans la bibliothèque.")
